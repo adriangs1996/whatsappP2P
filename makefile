f9c489373ccc7486d825347306d1c32a0ec0a5bb
adrian <a.sanchez3@estudiantes.matcom.uh.cu>
2019-12-06 00:09:40 -0500

    Arreglando el makefile y el testunit

--- makefile
+++ makefile
@@ -1,17 +1,29 @@
 CHORDPEER=tracker/dht/handler.py
-DEPENDENCIES=zmq cloudpickle
+DEPENDENCIES=zmq cloudpickle pyinstaller
 TRACKERLAUNCHER=tracker/launcher.py
 TRACKERLAUNCHERARGS=localhost:8000, localhost:8002, localhost:8005
+CLIENTDIR=client/
+CLIENTEXE=test.py
 
 .PHONY: test
 test:
 	python $(CHORDPEER) -i localhost -p 8000 &
+	sleep(1)
 	python $(CHORDPEER) -i localhost -p 8001 -t "localhost:8000" &
+	sleep(1)
 	python $(CHORDPEER) -i localhost -p 8002 -t "localhost:8000" &
+	sleep(1)
 	python $(CHORDPEER) -i localhost -p 8003 -t "localhost:8002" &
+	sleep(1)
 	python $(CHORDPEER) -i localhost -p 8004 -t "localhost:8001" &
+	sleep(1)
 	python $(CHORDPEER) -i localhost -p 8005 -t "localhost:8001" &
-	python $(TRACKERLAUNCHER) -i localhost -p 8080 -t $(TRACKERLAUNCHERARGS)
+	sleep(1)
+	python $(TRACKERLAUNCHER) -i localhost -p 8080 -t $(TRACKERLAUNCHERARGS) &
+	python $(TRACKERLAUNCHER) -i localhost -p 8888 -t $(TRACKERLAUNCHERARGS) &
+	cd $(CLIENTDIR)
+	python test.py &
+	cd ..
 
 .PHONY: clean
 clean:
@@ -21,3 +33,6 @@ clean:
 .PHONY: install
 install:
 	pip install $(DEPENDENCIES)
+	# Make a executable app
+	cd $(CLIENTDIR)
+	pyinstaller --onefile $(CLIENTEXE)
--- tracker/testunit.py
+++ tracker/testunit.py
@@ -53,7 +53,7 @@ class DHTTest(unittest.TestCase):
                 )
             )
             sleep(1)
-        
+
         for h in seeds:
             print(f"retrieving {h}")
             self.assertEqual(
@@ -66,60 +66,6 @@ class DHTTest(unittest.TestCase):
                 )
             )
             sleep(1)
-
-    # def test_dht_consistency(self):
-    #     print("Testing for consistency in DHT")
-    #     # Create a Random DHT of 20 nodes
-    #     address_list = [
-    #         ("127.0.0.1", x) for x in set(
-    #             map(lambda y: randrange(7000, 8000),
-    #                 range(10))
-    #         )
-    #     ]
-    #     nodes = []
-    #     nodes.append(Node(address_list[0][0], address_list[0][1]))
-    #     nodes[0].start_service()  # Bootstrap node
-    #     for host, port in address_list[1:]:
-    #         print("Creating node: %s:%s" % (host, port))
-    #         i = randrange(len(nodes))
-    #         n = Node(host, port, dest_host=(nodes[i].ip, nodes[i].port))
-    #         nodes.append(n)
-    #         n.start_service()
-    #         sleep(0.3)
-    #     # Give time for stabilization
-    #     for i in range(400):
-    #         print("Stabilization time " + f"{int(i/400 * 100)}%", end='\r')
-    #         sleep(0.1)
-    #     print("\nDone")
-
-    #     print("Seeding DHT with some Keys")
-    #     # Generate random addresess and hash them
-    #     seeds = [
-    #         ('12.10.92.87', x) for x in set(
-    #             map(lambda y: randrange(8000, 9000), range(6))
-    #         )
-    #     ]
-    #     hashes = [
-    #         int(
-    #             sha1(
-    #                 bytes("%s:%d" % x, 'ascii')).hexdigest(),
-    #             16
-    #         )
-    #         for x in seeds
-    #     ]
-    #     for h in hashes:
-    #         print(f"Putting {h}")
-    #         i = randrange(len(nodes))
-    #         node = RemoteNodeReference(nodes[i].ip, nodes[i].port)
-    #         node.put(h, "Hello There")
-    #         self.assertTrue(True)
-    #         print("Putted")
-    #         sleep(3)
-
-    #     print("Correctly added keys")
-    #     print("Waiting to stabilize again")
-    #     sleep(3)
-
 
 if __name__ == '__main__':
     unittest.main()
